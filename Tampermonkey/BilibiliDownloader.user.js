37639c171709cafe858e7d67dc8a14eeee6f7d56
Ye_Tianshun <admin@yts233.tk>
2021-02-16 19:42:02 +0800

    backup

--- Tampermonkey/BilibiliDownloader.user.js
+++ Tampermonkey/BilibiliDownloader.user.js
@@ -14,19 +14,20 @@
     var aid = video.aid;
     var bvid = video.bvid;
     var cid = video.cid;
-    var getPlayUrlApi = async(cid, bvid, quality) => (await(await fetch(`https://api.bilibili.com/x/player/playurl?cid=${cid}&bvid=${bvid}&qn=${quality}`)).json()).data;
-    var qualities = (await getPlayUrlApi(cid, bvid, 32)).accept_quality;
+    var getPlayUrlApi = async(quality) => (await(await fetch(`https://api.bilibili.com/x/player/playurl?cid=${cid}&bvid=${bvid}&qn=${quality}`)).json()).data;
+    var qualities = (await getPlayUrlApi(32)).accept_quality;
     window.downloader = {
         aid: aid,
         bvid: bvid,
         cid: cid,
+        title: video.title,
         qualities: qualities,
-        getPlayUrl: async function (quality) {
-            return (await getPlayUrlApi(cid, bvid, quality)).durl[0].url;
+        getPlayUrl: async function (quality = qualities[0]) {
+            return (await getPlayUrlApi(quality)).durl[0].url;
         },
-        download: async function (quality) {
+        download: async function (quality = qualities[0]) {
             var actor = document.createElement('a');
-            actor.href = (await getPlayUrlApi(cid.bvid, quality)).durl[0].url;
+            actor.href = (await getPlayUrlApi(quality)).durl[0].url;
             actor.download = `${video.title}.${aid}.flv`;
             actor.click();
         }
